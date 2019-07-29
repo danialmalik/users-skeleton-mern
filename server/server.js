3ccf242a411ddb6350db674ac7ee219e1c3d931a
danialmalik <danialmalik321@gmail.com>
2019-07-29 14:48:01 +0500

    simple hello world app

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import path from 'path'
+
+import express from 'express'
+import { MongoClient } from 'mongodb'
+
+import devBundle from './devBundle'
+import template from './../template'
+
+
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
+let port = process.env.PORT || 3000
+const CURRENT_WORKING_DIR = process.cwd()
+
+MongoClient.connect(url, (err, db)=>{
+  console.log("Connected successfully to mongodb server")
+  db.close()
+})
+
+const app = express()
+
+devBundle.compile(app)
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+app.get('/', (req, res) => {
+    res.status(200).send(template())
+})
+
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', port)
+})
