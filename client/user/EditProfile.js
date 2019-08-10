6868c890601b1952b372593077956cf5341191e0
danialmalik <danialmalik321@gmail.com>
2019-08-10 10:20:01 +0500

    fixed routes issue

--- client/user/EditProfile.js
+++ client/user/EditProfile.js
@@ -94,9 +94,32 @@ class EditProfile extends Component {
                     <Typography type="headline" component="h2" className={classes.title}>
                         Edit Profile
                     </Typography>
-                    <TextField id="name" label="Name" className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin="normal" /><br />
-                    <TextField id="email" type="email" label="Email" className={classes.textField} value={this.state.email} onChange={this.handleChange('email')} margin="normal" /><br />
-                    <TextField id="password" type="password" label="Password" className={classes.textField} value={this.state.password} onChange={this.handleChange('password')} margin="normal" />
+                    <TextField
+                        id="name"
+                        label="Name"
+                        className={classes.textField}
+                        value={this.state.name}
+                        onChange={this.handleChange('name')}
+                        margin="normal" />
+                    <br />
+
+                    <TextField
+                        id="email"
+                        type="email"
+                        label="Email"
+                        className={classes.textField}
+                        value={this.state.email}
+                        onChange={this.handleChange('email')}
+                        margin="normal" />
+                    <br />
+                    <TextField
+                        id="password"
+                        type="password"
+                        label="Password"
+                        className={classes.textField}
+                        value={this.state.password}
+                        onChange={this.handleChange('password')}
+                        margin="normal" />
                     <br /> {
                         this.state.error && (<Typography component="p" color="error">
                             <Icon color="error" className={classes.error}>error</Icon>
@@ -105,7 +128,9 @@ class EditProfile extends Component {
                     }
                 </CardContent>
                 <CardActions>
-                    <Button color="primary" variant="raised" onClick={this.clickSubmit} className={classes.submit}>Submit</Button>
+                    <Button color="primary" variant="raised" onClick={this.clickSubmit} className={classes.submit}>
+                        Submit
+                    </Button>
                 </CardActions>
             </Card>
         );
--- server/express.js
+++ server/express.js
@@ -7,6 +7,18 @@ import compress from 'compression';
 import cors from 'cors';
 import helmet from 'helmet';
 
+// modules for server side rendering
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import StaticRouter from 'react-router-dom/StaticRouter';
+
+import { SheetsRegistry } from 'react-jss/lib/jss';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { MuiThemeProvider, createMuiTheme, createGenerateClassName } from 'material-ui/styles';
+import { indigo, pink } from 'material-ui/colors';
+
+import MainRouter from './../client/MainRouter';
+
 import devBundle from './devBundle';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
@@ -21,7 +33,6 @@ const app = express();
  */
 devBundle.compile(app);
 
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,11 +41,51 @@ app.use(compress());
 app.use(helmet());
 app.use(cors());
 
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+
 app.use('/', userRoutes);
 app.use('/', authRoutes);
 
-app.get('/', (req, res, next) => {
-    res.send(Template());
+app.get('*', (req, res) => {
+    const sheetsRegistry = new SheetsRegistry();
+    const theme = createMuiTheme({
+        palette: {
+            primary: {
+                light: '#757de8',
+                main: '#3f51b5',
+                dark: '#002984',
+                contrastText: '#fff',
+            },
+            secondary: {
+                light: '#ff79b0',
+                main: '#ff4081',
+                dark: '#c60055',
+                contrastText: '#000',
+            },
+            openTitle: indigo['400'],
+            protectedTitle: pink['400'],
+            type: 'light'
+        },
+    });
+    const generateClassName = createGenerateClassName();
+    const context = {};
+    const markup = ReactDOMServer.renderToString(
+        <StaticRouter location={req.url} context={context}>
+            <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
+                <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
+                    <MainRouter />
+                </MuiThemeProvider>
+            </JssProvider>
+        </StaticRouter>
+    );
+    if (context.url) {
+        return res.redirect(303, context.url);
+    }
+    const css = sheetsRegistry.toString();
+    res.status(200).send(Template({
+        markup: markup,
+        css: css
+    }));
 });
 
 app.use((err, req, res, next) => {
